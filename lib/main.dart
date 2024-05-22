66bf90e08ed39a3ce1d8750bb5b6b4f6171eea7a
Marcelo Moreto <mmoreto@gmail.com>
2024-05-22 16:40:53 -0300

    initial cmm

---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(title: 'Image Picker Demo', home: MyHomePage(title: 'Image Picker Example'));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, this.title});
+
+  final String? title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<XFile>? _mediaFileList;
+
+  void _setImageFileListFromFile(XFile? value) {
+    _mediaFileList = value == null ? null : <XFile>[value];
+  }
+
+  dynamic _pickImageError;
+  // bool isVideo = false;
+
+  // VideoPlayerController? _controller;
+  // VideoPlayerController? _toBeDisposed;
+  String? _retrieveDataError;
+
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _onImageButtonPressed(
+    ImageSource source, {
+    required BuildContext context,
+  }) async {
+    if (context.mounted) {
+      try {
+        final XFile? pickedFile = await _picker.pickImage(source: source);
+        setState(() {
+          _setImageFileListFromFile(pickedFile);
+        });
+      } catch (e) {
+        setState(() {
+          _pickImageError = e;
+        });
+      }
+      // });
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget _previewImages() {
+    final Text? retrieveError = _getRetrieveErrorWidget();
+    if (retrieveError != null) {
+      return retrieveError;
+    }
+    if (_mediaFileList != null) {
+      return Semantics(
+        label: 'image_picker_example_picked_images',
+        child: ListView.builder(
+          key: UniqueKey(),
+          itemBuilder: (BuildContext context, int index) {
+            // final String? mime = lookupMimeType(_mediaFileList![index].path);
+
+            return Semantics(
+                label: 'image_picker_example_picked_image',
+                child: Image.file(
+                  File(_mediaFileList![index].path),
+                  errorBuilder: (BuildContext context, Object error, StackTrace? stackTrace) {
+                    return const Center(child: Text('This image type is not supported'));
+                  },
+                ));
+          },
+          itemCount: _mediaFileList!.length,
+        ),
+      );
+    } else if (_pickImageError != null) {
+      return Text('Pick image error: $_pickImageError', textAlign: TextAlign.center);
+    } else {
+      return const Text('You have not yet picked an image.', textAlign: TextAlign.center);
+    }
+  }
+
+  Widget _handlePreview() {
+    return _previewImages();
+  }
+
+  Future<void> retrieveLostData() async {
+    final LostDataResponse response = await _picker.retrieveLostData();
+    if (response.isEmpty) {
+      return;
+    }
+    if (response.file != null) {
+      //  isVideo = false;
+      setState(() {
+        if (response.files == null) {
+          _setImageFileListFromFile(response.file);
+        } else {
+          _mediaFileList = response.files;
+        }
+      });
+    } else {
+      _retrieveDataError = response.exception!.code;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title!)),
+      body: Center(
+        child: !kIsWeb && defaultTargetPlatform == TargetPlatform.android
+            ? FutureBuilder<void>(
+                future: retrieveLostData(),
+                builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return const Text('You have not yet picked an image.', textAlign: TextAlign.center);
+                    case ConnectionState.done:
+                      return _handlePreview();
+                    case ConnectionState.active:
+                      if (snapshot.hasError) {
+                        return Text('Pick image/video error: ${snapshot.error}}', textAlign: TextAlign.center);
+                      } else {
+                        return const Text('You have not yet picked an image.', textAlign: TextAlign.center);
+                      }
+                  }
+                },
+              )
+            : _handlePreview(),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          if (_picker.supportsImageSource(ImageSource.camera))
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: FloatingActionButton(
+                onPressed: () {
+                  // isVideo = false;
+                  _onImageButtonPressed(ImageSource.camera, context: context);
+                },
+                heroTag: 'image2',
+                tooltip: 'Take a Photo',
+                child: const Icon(Icons.camera_alt),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Text? _getRetrieveErrorWidget() {
+    if (_retrieveDataError != null) {
+      final Text result = Text(_retrieveDataError!);
+      _retrieveDataError = null;
+      return result;
+    }
+    return null;
+  }
+}
+
+// typedef OnPickImageCallback = void Function(double? maxWidth, double? maxHeight, int? quality, int? limit);
